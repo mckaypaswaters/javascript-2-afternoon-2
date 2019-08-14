162ece9aca9abd0be838690bd4ce9832306fe4e6
McKay Waters <mckaypaswaters@gmail.com>
2019-08-14 15:31:01 -0600

    Finished project 2

--- practice.js
+++ practice.js
@@ -18,8 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
-
+function first(arr){
+  return arr[0]
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,8 +34,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
-
+function last(arr){
+  return arr[2]
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +51,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for (i = 0; i < family.length; i++){
+    alert(family[i])
+  } 
+}
+console.log(family)
 
 ////////// PROBLEM 4 //////////
 
@@ -64,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters) {
+  for (i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -78,11 +89,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
-
-
-
+function evenFinder(nums){
+  let evenNums = []
+  for (i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0){
+    evenNums.push(nums[i])
+    }
+  }
+  return evenNums
+}
 
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
